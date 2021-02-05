1890b3a7aa138d743258e6bb814574e8cc0b175d
秦艳梅 <qinyanmei@yazuishou.com>
2021-02-05 15:49:33 +0800

    资源路径

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   devServer: {
     inline: true,
     disableHostCheck: true,
+    publicPath:'/vue3-element-plus-admin/',
     proxy: {
       '/api': {
         target: 'http://localhost:8080/',
