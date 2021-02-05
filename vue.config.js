758bd8c1e5c25a5a00ca16b6ec5d949f0c54ddbf
秦艳梅 <qinyanmei@yazuishou.com>
2021-02-05 16:09:07 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
+  publicPath:'/vue3-element-plus-admin/',
   devServer: {
     inline: true,
     disableHostCheck: true,
-    publicPath:'/vue3-element-plus-admin/',
     proxy: {
       '/api': {
         target: 'http://localhost:8080/',
